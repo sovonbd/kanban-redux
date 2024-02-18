e1f062e0a461177e564180cfcbad9aaf9a984101
sovonbd <sovonsdp@gmail.com>
2024-02-18 12:52:32 -0600

    updated

---
+++ src/redux/features/tasks/tasksSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  tasks: [],
+};
+const tasksSlice = createSlice({
+  name: "tasksSlice",
+  initialState,
+  reducers: {},
+});
+
+export default tasksSlice.reducer;
