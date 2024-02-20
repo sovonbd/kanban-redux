fc9ce20804a01d5a44aab3cb22fd1aa6397ee2c1
sovonbd <sovonsdp@gmail.com>
2024-02-19 20:17:27 -0600

    updated

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,7 +2,9 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { userTasks } from "../../redux/features/tasks/tasksSlice";
 
 const MyTasks = () => {
   const item = {
@@ -17,7 +19,11 @@ const MyTasks = () => {
   };
 
   const { tasks } = useSelector((state) => state.tasksSlice);
-
+  const { name: userName } = useSelector((state) => state.userSlice);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(userTasks(userName));
+  }, [userName, dispatch, tasks]);
   return (
     <div>
       <h1 className="text-xl my-3">My Tasks</h1>
