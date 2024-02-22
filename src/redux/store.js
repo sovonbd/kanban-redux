ea877484435d40adcea94467c1d4370ffc6aa96b
sovonbd <sovonsdp@gmail.com>
2024-02-21 21:08:47 -0600

    updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import tasksSlice from "./features/tasks/tasksSlice";
 import userSlice from "./user/userSlice";
 
-const store = configureStore({
-  reducer: {
-    tasksSlice: tasksSlice,
-    userSlice: userSlice,
-  },
-});
 
 export default store;
