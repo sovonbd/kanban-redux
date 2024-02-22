99c34ce0fada1b61c5cb63491077c61df73858d6
sovonbd <sovonsdp@gmail.com>
2024-02-21 21:01:02 -0600

    updated

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -12,9 +12,17 @@ const baseApi = createApi({
     getPostById: builder.query({
       query: (id) => `/posts/${id}`,
     }),
+    postData: builder.mutation({
+      query: (post) => ({
+        url: "/posts",
+        method: "POST",
+        body: post,
+      }),
+    }),
   }),
 });
 
-export const { useGetPostsQuery, useGetPostByIdQuery } = baseApi;
+export const { useGetPostsQuery, useGetPostByIdQuery, usePostDataMutation } =
+  baseApi;
 
 export default baseApi;
