6ff0cee153db09af95bb64b6e8fb027411f8305e
sovonbd <sovonsdp@gmail.com>
2024-02-21 18:52:15 -0600

    updated

---
+++ src/redux/features/api/baseApi.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com/",
+  }),
+  endpoints: {},
+});
+
+export default baseApi;
---
+++ src/redux/store1.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store1 = configureStore({
+  reducer: {},
+});
+
+export default store1;
