ea2204e347f55dc048f249f635ba023bfbe86042
sovonbd <sovonsdp@gmail.com>
2024-02-22 22:17:45 -0600

    updated

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -67,13 +67,7 @@ const AddTaskModal = ({ isOpen, setIsOpen }) => {
             <option value="Mizanur Rahman">Mizanur Rahman</option>
             <option value="Tanmoy Parvez">Tanmoy Parvez</option>
             <option value="Fahim Ahmed Firoz">Fahim Ahmed Firoz</option>
-            <option value="Rahatul Islam">Rahatul Islam</option>
-            <option value="Samin Israr Ravi">Samin Israr Ravi</option>
-            <option value="Mehedi Anik">Mehedi Anik</option>
-            <option value="Ehtisam Haq">Ehtisam Haq</option>
-            <option value="Anisur Rahman">Anisur Rahman</option>
-            <option value="Muktadir Hasan">Muktadir Hasan</option>
-            <option value="Masud Alam">Masud Alam</option>
+       
           </select>
         </div>
         <div className="flex flex-col mb-5">
